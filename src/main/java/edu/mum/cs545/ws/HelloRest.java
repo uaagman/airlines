88381a7d10922e139d4795bfb906baf7c3f82e46
uaagman <uaagman05neu@gmail.com>
2017-06-08 17:16:36 -0500

    try to fix problem

--- src/main/java/edu/mum/cs545/ws/HelloRest.java
+++ src/main/java/edu/mum/cs545/ws/HelloRest.java
@@ -5,10 +5,9 @@ import cs545.airline.service.AirlineService;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
 
 @Named
 @Path("hello")
@@ -18,19 +17,29 @@ public class HelloRest {
     private AirlineService airlineService;
 
     @GET
-    public String helloWorld(@DefaultValue("Gorgeous") @QueryParam("name") String name) {
-        return "Hello " + name + "!";
+    public Response helloWorld(@DefaultValue("Gorgeous") @QueryParam("name") String name) {
+        String  result = "Hello " + name + "!";
+        return Response.status(200).entity(result).build();
     }
 
     @Path("airline")
     @GET
-    public String getAirlineTest() {
+    public Response getAirlineTest() {
         String result = "Nil!";
         Airline airline = airlineService.findByName("oneworld");
         if (airline != null) {
             result = "This is an airline: " + airline.getName();
         }
-        return result;
+        return Response.status(200).entity(airline).build();
+    }
+
+    @Path("saveAirline")
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    public Response saveAirline(final MultivaluedMap<String, String> formParams){
+        airlineService.create(new Airline(formParams.getFirst("name")));
+
+        return Response.status(200).entity(airlineService.findByName(formParams.getFirst("name"))).build();
     }
 
 }
